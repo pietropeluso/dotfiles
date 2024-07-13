3dd84a034dc8cc5a65b7b7c83fe3e9d3a1602db7
Pietro Peluso <pietro@MacBook-Air.localdomain>
2024-07-13 16:41:14 +0100

    Add more brew packages

--- brew.sh
+++ brew.sh
@@ -3,16 +3,39 @@
 # Make sure homebrew is installed first
 if [[ ! "$(type -P brew)" ]]; then
     echo "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew tap caskroom/cask
 brew doctor
 brew update
 
-brew install git
-brew install vim
 brew install htop
-brew install yarn
 brew install tree
-brew install diff-so-fancy
+
+# Browser installation 
+brew install --cask firefox
+brew install --cask arc
+
+# Software development tools
+brew install nvm
+brew install docker
+brew install docker-compose
+brew install --cask visual-studio-code
+
+# Messaging application
+brew install --cask whatsapp
+brew install --cask signal
+brew install --cask zoom
+brew install --cask slack
+
+# Desktop and window management
+brew install --cask caffeine
+brew install --cask rectangle
+
+# Utilities
+brew install --cask chatgpt
+brew install --cask notion
+brew install --cask adobe-acrobat-reader
+brew install --cask 1password
+
